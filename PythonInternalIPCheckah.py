b15a9b6b301c5188cc55af99101d3ccf8c79deb3
Evan Booth <e.booth@odigia.com>
2012-11-26 13:20:19 -0500

    moved bcolors out into separate module

--- PythonInternalIPCheckah.py
+++ PythonInternalIPCheckah.py
@@ -2,21 +2,7 @@
 
 import httplib, sys, urllib2, argparse
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-
-    def disable(self):
-        self.HEADER = ''
-        self.OKBLUE = ''
-        self.OKGREEN = ''
-        self.WARNING = ''
-        self.FAIL = ''
-        self.ENDC = ''
+from bcolors import bcolors
 
 parser = argparse.ArgumentParser(description='Check for IIS internal IP leakage. CALL TH AMBER LAMPS.')
 parser.add_argument('host', metavar='www.asp.net', help='target host')
